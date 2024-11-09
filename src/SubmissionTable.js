7011e9316e079c3e6cd4402eba996a058d135738
Rochelle Li <rochelleli@Rochelles-MacBook-Pro.local>
2024-11-09 16:58:52 -0600

    Added submission stuff

--- src/SubmissionTable.js
+++ src/SubmissionTable.js
@@ -2,25 +2,14 @@ import React from "react";
 import { useStyletron } from "baseui";
 import {
   StatefulDataTable,
-  BooleanColumn,
-  CategoricalColumn,
   CustomColumn,
-  NumericalColumn,
   StringColumn,
+NumericalColumn,
   COLUMNS,
-  NUMERICAL_FORMATS,
 } from "baseui/data-table";
 
-// https://gist.github.com/6174/6062387
-function pseudoRandomString(rowIdx, columnIdx) {
-  return (
-    (0.88 * rowIdx).toString(36).replace(".", "").substring(2) +
-    (0.99 * columnIdx).toString(36).replace(".", "")
-  ).slice(0, 10);
-}
-
 function transformData(columns, d) {
-    const data = d['data']
+const data = d['data']
   const rowCount = data.length;
   const rows = [];
   for (let i = 0; i < rowCount; i++) {
@@ -41,10 +30,15 @@ function transformData(columns, d) {
                     default:
                         return { color: "red" , status: "Error"};
                 }
-          case COLUMNS.STRING:
+        case COLUMNS.STRING:
+            if (column.title === "Runtime") {
                 return Math.round(runtime * 1000 * 100) / 100;
+            }
+            return error;
+        case COLUMNS.NUMERICAL:
+            return n;
           default:
-            return "default" + pseudoRandomString(i, j);
+            return "default";
         }
       }),
     });
@@ -76,14 +70,25 @@ const columns = [
         title: "Runtime",
         mapDataToValue: (data) => `${data[1].toString()} ms`,
     }),
+    StringColumn({
+        title: "Error",
+        maxWidth: 300,
+        lineClamp: 3,
+        mapDataToValue: (data) => `${data[2].toString()}`,
+    }),
+    NumericalColumn({
+        title: "Test cases passed",
+        mapDataToValue: (data) => data[3],
+    }),
   
 ];
 
 export default function SubmissionTable(data) {
   const [css] = useStyletron();
+
   return (
-    <div className={css({ height: "600px" })}>
-      <StatefulDataTable columns={columns} rows={transformData(columns, data)} />
+    <div className={css({ height: "300px" })}>
+      <StatefulDataTable rowHeight={78} filterable={false} searchable={false} columns={columns} rows={transformData(columns, data)} />
     </div>
   );
 }
